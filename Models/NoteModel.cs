f1a7f40b4565530692893bf354a8c6ccef6f8a78
capogluuu <capogluuu@gmail.com>
2022-02-17 21:39:24 +0300

    Add project files.

---
+++ Models/NoteModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace obss1.Models
+{
+    public class NoteModel
+    {
+        private string CourseName { get; set; }
+        private int FirstExam { get; set; }
+        private int SecondExam { get; set; }
+        private bool PassState { get; set; }
+
+        public NoteModel() { }
+
+        public NoteModel(string _courseName, int _firstExam, int _secondExam, bool _passState)
+        {
+            CourseName = _courseName;
+            FirstExam = _firstExam;
+            SecondExam = _secondExam;
+            PassState = _passState;
+
+        }
+        public string courseName { get { return CourseName; } set { CourseName = value; } }
+        public int firstExam { get { return FirstExam; } set { FirstExam = value; } }
+        public int secondExam { get { return SecondExam; } set { SecondExam = value; } }
+        public bool passState { get { return PassState; } set { PassState = value; } }
+
+    }
+}
---
+++ Models/StudentModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace obss1.Models
+{
+    public class StudentModel
+    {
+        private Guid Id { get; set; }
+        private string Name { get; set; }
+        private string Surname { get; set; }
+        private string StudentNumber { get; set; }
+        private List<NoteModel> Notes { get; set; }
+        
+        public StudentModel() { }
+        public StudentModel(string _name, string _surname, string _studentNumber, List<NoteModel> _notes)
+        {
+            Name = _name;
+            Surname = _surname;
+            StudentNumber = _studentNumber;
+            Notes = _notes;
+        }
+
+        [Key]
+        public Guid id { get { return Id; } set { Id = value; } }
+        public string name { get { return Name; } set { Name = value; } }
+        public string surname { get { return Surname; } set { Surname = value; } }
+        public string studentNumber { get { return StudentNumber; } set { StudentNumber = value; } }
+        public List<NoteModel> notes { get { return Notes; } set { Notes = value; } }
+
+
+    }
+}
